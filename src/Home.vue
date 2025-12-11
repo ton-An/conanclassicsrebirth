2e9fc2fc8f092f678eab06da4ddd30f57690ea68
Anton <anton@antons-webfabrik.eu>
2025-12-11 16:04:48 +0100

    feat: add title to home page

--- src/Home.vue
+++ src/Home.vue
@@ -6,13 +6,17 @@ import SearchResults from './components/SearchResults.vue'
 
 <template>
   <div class="flex flex-col items-center justify-center gap-8 p-4 min-h-screen">
-    <img
-      width="100"
-      height="100"
-      src="@/assets/hair.png"
-      alt="Conan Classics Search"
-      class="logo"
-    />
+    <div class="flex flex-col items-center justify-center gap-4">
+      <img
+        width="100"
+        height="100"
+        src="@/assets/hair.png"
+        alt="Conan Classics Search"
+        class="logo"
+      />
+
+      <h1 class="text-2xl font-medium">Conan Classics Search</h1>
+    </div>
     <SearchBar />
     <SearchResults />
     <FaqSheet />
